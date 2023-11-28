35f67cd34e81aa4459a062ebbf55e2006854f38f
CECILE ADAM <cecileadammorvan@gmail.com>
2023-11-28 17:26:38 +0100

    avancée user story 8

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -6,8 +6,14 @@ class ProposalsController < ApplicationController
 
   def create
     @proposal = Proposal.new
+    # je dois récupérer la new
+    # je dois récupérer l'id de la question
+    # id du current user
   end
+
   def index
     # je peux voir toutes les réponses sauf la mienne
+    @proposals = Proposal.all
+    # sauf celle du current user
   end
 end
