741c66ca17b331770392a636904a3287566c266e
catriona beamish <francois.lerosier@gmail.com>
2023-11-28 17:57:58 +0100

    Carouselstep1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_134835) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_155611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_134835) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.bigint "user_id"
+    t.integer "selected_avatar"
     t.index ["user_id"], name: "index_avatars_on_user_id"
   end
 
