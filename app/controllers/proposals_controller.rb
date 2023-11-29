ccabe2b47033d22d839661d0ae2e6d08df176557
CECILE ADAM <cecileadammorvan@gmail.com>
2023-11-29 14:56:05 +0100

    add proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,23 +2,38 @@ class ProposalsController < ApplicationController
   def new
     @proposal = Proposal.new
     @question = Question.find(params[:question_id])
+    @game = Game.find(params[:game_id])
   end
 
   def create
     # je récupère la réponse du user
-    @proposal = Proposal.new
+    @proposal = Proposal.new(proposal_params)
+    @proposal.user = current_user
+    question = Question.find(params[:question_id])
+    @proposal.question = question
+    game = Game.find(params[:game_id])
+    @proposal.game = game
     # game id
     if @proposal.save
-      redirect_to @proposal
+      redirect_to game_question_path(game, question)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def index
-    # je peux voir toutes les réponses sauf la mienne
-    @proposals = Proposal.where.not(user: current_user)
-    # sauf celle du current user
+  # def index
+  #   # @question = Question.find(params[:question_id])
+  #   # @game = Game.find(params[:game_id])
+  #   # @proposals = @game.questions.proposals
+  #   # je peux voir toutes les réponses sauf la mienne
+  #   # @proposals = Proposal.where.not(user: current_user)
+  #   # sauf celle du current user
+  # end
+
+  private
+
+  def proposal_params
+    # Define the permitted parameters here
+    params.require(:proposal).permit(:proposal)
   end
-
-  
-
 end
