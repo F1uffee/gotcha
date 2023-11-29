591fb0df952257234f061432a7814ded8a181bd3
CECILE ADAM <cecileadammorvan@gmail.com>
2023-11-29 17:14:52 +0100

    add new chgt

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,10 @@ class QuestionsController < ApplicationController
     @all = @question.proposals.where(user_id: current_user.id)
     # @user_answer_proposal = @user_answer.proposal.to_s
     @array = @all.to_a
-    @array_of_proposal = @array.map{ |proposal| proposal.proposal}
-    @final_array = @array_of_proposal << @question.reponse
-    @shuffled_array = @final_array.shuffle
+    @array << @question
+    @shuffled_array = @array.shuffle!
+    # @final_array = @array_of_proposal << @question.reponse
+    # @shuffled_array = @final_array.shuffle
 
     # @array_without_mine = @final_array
   end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,14 @@
 class VotesController < ApplicationController
+
+  def create
+    # @proposal = Proposal.find(params[:id])
+    # @vote = current_user.votes(proposal: @proposal)
+    
+
+    if @vote.save
+      # alors afficher l'icone sur la question et incrémenter score
+    else
+      # render
+    end
+  end
 end
