9ce7a3e41777dddbc56f1a2ce3c5c4bc8697a0c2
CECILE ADAM <cecileadammorvan@gmail.com>
2023-12-04 14:13:02 +0100

    add front monday

--- app/javascript/controllers/autolink_controller.js
+++ app/javascript/controllers/autolink_controller.js
@@ -2,10 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autolink"
 export default class extends Controller {
-  static targets = ["link", "link_two", "link_three"]
+  static targets = ["link_two", "link_three"]
 
   connect() {
-    console.log(this.linkTarget);
+    // console.log(this.linkTarget);
     const delayInMillis = 10000;
     const delayInMillis_two = 20000;
     setTimeout(() =>this.linkTarget.click(), delayInMillis);
