af293d15809b3b95291030d99e642d3994f99408
catriona beamish <francois.lerosier@gmail.com>
2023-12-06 12:40:15 +0100

    new timer bar

--- app/javascript/controllers/autolink_controller.js
+++ app/javascript/controllers/autolink_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
 
   connect() {
     // console.log(this.linkTarget);
-    const delayInMillis = 7000;
+    const delayInMillis = 70000;
     const delayInMillis_two = 20000;
     setTimeout(() =>this.linkTarget.click(), delayInMillis);
     setTimeout(() =>this.link_twoTarget.click(), delayInMillis);
