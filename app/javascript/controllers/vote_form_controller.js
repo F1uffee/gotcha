8be9b338a5f0673fa61924f477714a4bb687f325
Antoine Le Floch <antoine.lefloch@etik.com>
2023-12-06 15:25:36 +0100

    en attente des autres joueurs après le vote

--- app/javascript/controllers/vote_form_controller.js
+++ app/javascript/controllers/vote_form_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="vote-form"
 export default class extends Controller {
+  static targets = ["decorableElement"]
+
 
   connect() {
     console.log("coucou")
